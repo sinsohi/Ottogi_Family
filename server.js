e8c8d51cfea10f91f012bd5e65d486ea59ed0520
KimYeYoung125 <pindoll76@naver.com>
2024-05-13 17:38:01 +0900

    [FIX]로컬 브랜치 병합 오류 수정

--- server.js
+++ server.js
@@ -4,15 +4,10 @@ const path = require('path') // 추가
 const bodyParser = require('body-parser'); //npm install body-parser
 const bcrypt = require('bcrypt') // bcrypt 셋팅
 const MongoStore = require("connect-mongo"); // connect-mongo 셋팅
-
 require("dotenv").config(); // .env 파일에 환경변수 보관
-
 app.use(express.static(__dirname + '/public'))
 // app.use(express.static('public')); 
-
-
 //추가
-
 app.use(
   '/build/',
   express.static(path.join(
@@ -20,7 +15,6 @@ app.use(
     'node_modules/three/build'
   ))
 )
-
 app.use(
   '/jsm/',
   express.static(path.join(
@@ -28,24 +22,18 @@ app.use(
     'node_modules/three/examples/jsm'
   ))
 )
-
 // body-parser 미들웨어 사용 설정
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-
 // 템플릿엔진 ejs 셋팅
 app.set('view engine','ejs')
-
 // request.body 쓰기 위한 코드
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // passport 라이브러리 셋팅
 const session = require('express-session')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
-
 app.use(passport.initialize())
 app.use(session({
   secret: '암호화에 쓸 비번',
@@ -57,13 +45,9 @@ app.use(session({
     dbName:"Ottogi_Family"
   })
 }))
-
 app.use(passport.session())
-
-
 // mongoDB 연결
 const { MongoClient, ObjectId } = require('mongodb');
-
 let db;
 const url = process.env.DBurl;
 new MongoClient(url).connect().then((client)=>{
@@ -72,29 +56,19 @@ new MongoClient(url).connect().then((client)=>{
 }).catch((err)=>{
   console.log(err)
 })
-
-
-
 app.listen(process.env.PORT, ()=>{
     console.log('http://localhost:'+`${process.env.PORT}` +' 에서 서버 실행중')
 })
-
 app.get('/homePage',(request,response)=>{
   response.render('homePage.ejs');
 })
-
-
 // app.get('/homePage',(request,response)=>{
 //   response.sendFile(__dirname + '/index.html')
 // })
-
-
 app.get('/register',(request,response)=>{
   response.render('register.ejs');})
-
 app.post('/register', async (request,response)=>{
   let hash = await bcrypt.hash(request.body.password,10) // password hashing (암호화)
-
   await db.collection('user_info').insertOne({
     userNickname : request.body.userNickname,
     username : request.body.username,
@@ -102,8 +76,6 @@ app.post('/register', async (request,response)=>{
   })
   response.sendFile(__dirname + '/InitialScreen.html')
 })
-
-
 // 아이디/비번이 DB와 일치하는지 검증하는 로직 짜는 공간 (앞으로 유저가 제출한 아이디 비번이 DB랑 맞는지 검증하고 싶을때 이것만 실행하면 됨)
 passport.use(
   new LocalStrategy(async (입력한아이디, 입력한비번, cb) => {
@@ -112,7 +84,6 @@ passport.use(
   if (!result) {
     return cb(null, false, { message: '아이디 DB에 없음' })
   }
-
   // password 비교
   if (await bcrypt.compare(입력한비번, result.password)) {
     return cb(null, result)
@@ -120,10 +91,6 @@ passport.use(
     return cb(null, false, { message: '비번불일치' });
   }
 }))
-
-
-
-
 // 로그인시 세션 만들기 (요청.logIn() 쓰면 자동 실행됨)
 passport.serializeUser((user, done) => {
   // console.log(user);
@@ -132,7 +99,6 @@ passport.serializeUser((user, done) => {
     done(null, { id: user._id, username: user.username });
   });
 });
-
 // 유저가 보낸 쿠키 분석 (세션 정보 적힌 쿠키 가지고 있는 유저가 요청 날릴 때마다 실행됨)
 passport.deserializeUser(async (user, done) => {
   let result = await db
@@ -143,14 +109,11 @@ passport.deserializeUser(async (user, done) => {
     done(null, result); // result : 요청.user에 들어감
   });
 });
-
 app.get('/login',(request,response)=>{
   response.render('login.ejs')
 })
-
 // 아이디/비번이 DB와 일치하는지 검증하는 코드 
 app.post('/login', async (요청, 응답, next) => {
-
   passport.authenticate('local', (error, user, info) => {
     if (error) return 응답.status(500).json(error)
       if (!user) return 응답.status(401).json(info.message)
@@ -162,43 +125,33 @@ app.post('/login', async (요청, 응답, next) => {
     });
   })(요청, 응답, next);
 }) 
-
 app.get('/calender',(request,response)=>{
   response.sendFile(__dirname + '/calender.html')
 })
-
-
 app.get('/',(request,response)=>{
   response.sendFile(__dirname + '/InitialScreen.html')
 })
-
 app.get('/calendardetail',(request,response)=>{
   response.sendFile(__dirname + '/calendardetail.html')
 });
-
 app.get('/setting', (req, res) => {
   res.sendFile(__dirname + '/setting.html');
 }); // 세팅
-
 app.get('/daily-record', (req, res) => {
   res.sendFile(__dirname + '/daily-record.html');
 }); // 매일 기록
-
 app.post('/submit-form', (req, res) => {
   const meal = req.body.meal;
   res.send(`<script>window.location.href = "/dailyrecordmeal?meal=${meal}";</script>`);
 });
-
 app.get('/dailyrecordmeal', (req, res) => {
   res.sendFile(path.join(__dirname, 'dailyrecordmeal.html'));
 });
-
 app.post('/dailyrecordmeal', async (req, res) => {
   const meal = req.body.meal;
   const menuName = req.body.menuName;
   const calories = req.body.calories;
   const userNickname = req.user.userNickname; // 유저의 userNickname
-
   let collectionName;
   if (meal === 'breakfast') {
     collectionName = 'breakfast';
@@ -207,13 +160,11 @@ app.post('/dailyrecordmeal', async (req, res) => {
   } else if (meal === 'dinner') {
     collectionName = 'dinner';
   }
-
   const data = {
     userNickname: userNickname,
     menuName: menuName,
     calories: calories
   };
-
   db.collection(collectionName).insertOne(data, (err, result) => {
     if (err) {
       console.log('데이터베이스 오류:', err);
@@ -223,7 +174,6 @@ app.post('/dailyrecordmeal', async (req, res) => {
     res.status(200).send('성공적으로 제출');
   });
 });
-
 app.post('/setting', (req, res) => {
   const userNickname = req.user.userNickname;
   const gender = req.body.gender;
@@ -233,7 +183,7 @@ app.post('/setting', (req, res) => {
   const sleeptime = req.body.sleeptime;
   const activity = req.body.activity;
   const bmi = req.body.bmi;
-  
+
   const data = {
       userNickname: userNickname,
       gender: gender,
