57167bfc87bfc0b5c840ae83fe7ca469fd33a380
Sinsohi <sohi8451@naver.com>
2024-05-14 19:01:24 +0900

    ottogi.js 삭제 및 ottogi2.js 작성(Renderer, Camera)

---
+++ public/mainPage2.js
@@ -0,0 +1,3 @@
+import ottogi_module2 from "./ottogi2.js";
+
+ottogi_module2();
---
+++ public/ottogi2.js
@@ -0,0 +1,42 @@
+import * as THREE from 'three'
+
+export default function ottogi_module2(){
+    const canvas = document.querySelector('canvas.webgl');
+    const scene = new THREE.Scene()
+
+    // 배경 이미지
+    const loader = new THREE.TextureLoader();
+    loader.load('./img/HomePage.png', function(texture){
+        scene.background = texture;
+    });
+
+	// 화면 크기 설정
+    const sizes = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
+   
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({ canvas })
+
+    const render = () => {
+        renderer.setSize(sizes.width, sizes.height)
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+        renderer.render(scene, camera)
+    }
+
+    // Camera
+    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
+    camera.position.z = 8
+    scene.add(camera)
+
+	// 창 크기가 바뀔 때마다 카메라 비율 재조정
+    window.addEventListener('resize', () =>
+    {
+        sizes.width = window.innerWidth
+        sizes.height = window.innerHeight
+
+        camera.aspect = sizes.width / sizes.height
+        camera.updateProjectionMatrix()
+    })
+}
